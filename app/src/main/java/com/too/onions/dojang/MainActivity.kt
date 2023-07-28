df1ebbfa904ffd1d8f9ddeda5568db4aa4f8acfe
sjkim <ksj0402@nepes.co.kr>
2023-07-28 17:30:00 +0900

    update singleView

--- app/src/main/java/com/too/onions/dojang/MainActivity.kt
+++ app/src/main/java/com/too/onions/dojang/MainActivity.kt
@@ -1,12 +1,12 @@
 package com.too.onions.dojang
 
+import android.graphics.Paint.Align
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.Image
-import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.fillMaxHeight
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
@@ -16,6 +16,7 @@ import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material3.Surface
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
@@ -33,8 +34,7 @@ class MainActivity : ComponentActivity() {
                 Surface(
                     modifier = Modifier.fillMaxSize()
                 ) {
-                    DrawBG()
-                    DrawTitleBar()
+                    DrawSingleView()
                 }
             }
         }
@@ -42,27 +42,40 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun DrawBG() {
-    Image(
-        painterResource(id = R.drawable.bg_single),
-        contentDescription = null,
-        contentScale = ContentScale.FillBounds
-    )
+fun DrawSingleView() {
+    Box(
+        modifier = Modifier.fillMaxSize()
+    ) {
+        Image(
+            painterResource(id = R.drawable.bg_single),
+            contentDescription = null,
+            contentScale = ContentScale.FillBounds,
+            modifier = Modifier.fillMaxSize()
+        )
+
+        TitleBar()
+
+    }
+
 }
 
 @Composable
-fun DrawTitleBar() {
-    Box(
+fun TitleBar() {
+    Surface(
+        color = Color(0xfff2f1f3),
         modifier = Modifier
-            .width(340.dp)
+            .fillMaxWidth()
             .height(110.dp)
-            .background(color = Color(0xfff200f3))
     ) {
-        Box(modifier = Modifier.size(342.dp, 40.dp)) {
+        Row(
+            modifier = Modifier.size(342.dp, 40.dp)
+                .padding(vertical = 50.dp),
+            verticalAlignment = Alignment.Top
+        ) {
             Box(modifier = Modifier
-                .fillMaxHeight()
                 .width(254.dp)) {
                 Image(
+
                     painterResource(id = R.drawable.ic_launcher_foreground),
                     contentDescription = null,
                     modifier = Modifier.size(22.dp, 22.dp)
@@ -81,8 +94,7 @@ fun DefaultPreview() {
         Surface(
             modifier = Modifier.fillMaxSize()
         ) {
-            DrawBG()
-            DrawTitleBar()
+            DrawSingleView()
         }
     }
 }
